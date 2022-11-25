55756cd0e582abee6c31311f93da86144380077b
Nithin-Varma <mnithin1422@gmail.com>
2022-11-25 14:46:42 +0530

    login page, web3.js and metamask connection

--- pages/login.js
+++ pages/login.js
@@ -3,6 +3,31 @@ import tw from "tailwind-styled-components";
 import { useRouter } from "next/router";
 import { signInWithPopup, onAuthStateChanged } from "firebase/auth";
 import { auth, provider } from "../firebase";
+import Image from 'next/image';
+
+import Web3 from 'web3';
+import Web3Modal from 'web3modal';
+import WalletConnectProvider from "@walletconnect/web3-provider";
+// import { WEB3_CONNECT_MODAL_ID } from "web3modal";
+
+const provider_options = {
+	// walletconnect: {
+	// 	package:WalletConnectProvider,
+	// 	options: {
+	// 		infuraId
+	// 	}
+	// }
+
+}
+if(typeof window !== "undefined")
+{
+	const web3modal = new Web3Modal(
+		{
+		network : 'testnet',
+		cacheProvider: true,
+		provider_options,
+})
+}
 
 const Login = () => {
 	const router = useRouter();
@@ -17,14 +42,17 @@ const Login = () => {
 
 	return (
 		<Wrapper>
-			<UberLogo src="https://i.ibb.co/ZMhy8ws/uber-logo.png" />
+			<UberLogo src={"/logo.png"} alt="gyro logo" />
 
-			<Title>Log in to access your account</Title>
+			<Title>Login with metamask</Title>
 
-			<HeadImage src="https://i.ibb.co/CsV9RYZ/login-image.png" />
+			<HeadImage src="/head.png" />
 
-			<SignInButton onClick={() => signInWithPopup(auth, provider)}>
-				Sign in with Google
+			<SignInButton onClick={async () => {
+				const provider = await web3modal.connect();
+				const web3 = new Web3(provider);
+			}}>
+				Sign in with metamask
 			</SignInButton>
 		</Wrapper>
 	);
@@ -33,7 +61,8 @@ const Login = () => {
 export default Login;
 
 const Wrapper = tw.div`
-    flex flex-col h-screen w-screen bg-gray-200  p-4 
+    flex flex-col h-screen w-screen bg-white-200  p-4 
+	
 `;
 
 const UberLogo = tw.img`
@@ -41,7 +70,8 @@ const UberLogo = tw.img`
 `;
 
 const Title = tw.div`
-    text-5xl  pt-4 text-gray-500
+    text-5xl  pt-4 text-white-500
+	
 `;
 
 const HeadImage = tw.img`
@@ -49,5 +79,11 @@ const HeadImage = tw.img`
 `;
 
 const SignInButton = tw.button`
-    bg-black text-white text-center py-4 mt-8  w-full rounded-full self-center
+    // bg-black text-white text-center py-4 mt-8  w-full rounded-full self-center
+	bg-transparent hover:bg-purple-500 text-blue-400 font-bold hover:text-white py-2 px-4 border border-blue-900 hover:border-transparent rounded
 `;
+
+
+{/* <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+  Button
+</button> */}
